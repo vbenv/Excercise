1ab58f0d51dc6ef8dbd1f9d543ace7183da3f227
vbenv <tjdcksl20@gmail.com>
2024-03-08 14:10:25 +0900

    if_5-1

--- python/if.py
+++ python/if.py
@@ -19,14 +19,9 @@
 a,b,c = map(int, input().split(' '))
 if a == b == c:
     print(10000+(a*1000))
-elif a == b or b == c:
-    print(1000 + (b * 100))
-elif a == c:
+elif a == b or a == c:
     print(1000 + (a * 100))
+elif b == c:
+    print(1000 + (b * 100))
 else:
-    if a > b and a > c:
-        print(a*100)
-    elif b > c and b > a:
-        print(b*100)
-    else:
-        print(c*100)
+    print(max(a,b,c)*100)
